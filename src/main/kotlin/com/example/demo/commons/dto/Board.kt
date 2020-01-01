bd23bbd1ada7f5587ef0bf88c8e5aa35996f8322
Closet <closset703@nate.com>
2020-01-01 23:12:24 +0900

    보드 도메인을 초기 구성한다
    work items : none

---
+++ src/main/kotlin/com/example/demo/commons/dto/Board.kt
@@ -0,0 +1,21 @@
+package com.example.demo.commons.dto
+
+import com.example.demo.commons.vo.DateMetaMappedSuperClass
+
+class Board() : DateMetaMappedSuperClass() {
+    data class BoardCreateRequest(
+            val issuerId: String,
+            val title: String,
+            val content: String,
+            val password: String
+    )
+
+    data class ChangePasswordRequest(
+            val password: String
+    )
+
+    data class ChangeBoardRequest(
+            val content: String,
+            val title: String
+    )
+}
---
+++ src/test/kotlin/com/example/demo/domain/board/BoardServiceTest.kt
@@ -0,0 +1,16 @@
+@file:Suppress("NonAsciiCharacters")
+
+package com.example.demo.domain.board
+
+//class BoardServiceTest(
+//        val boardRepository: BoardRepository
+//) {
+//
+//    private val boardService: BoardService = BoardService(userRepository)
+//
+//
+//    fun `게시판 생성 요청에 성공한다`() {
+//        boardService.createBoard(boardCreateRequest)
+//    }
+//
+//}
