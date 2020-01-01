0e3a3af5ccc83944849e5dfdfb8c22f1edc6144a
Closet <closset703@nate.com>
2020-01-01 23:12:24 +0900

    보드 도메인을 초기 구성한다
    work items : none

---
+++ src/main/kotlin/com/example/demo/domain/board/BoardAggregate.kt
@@ -0,0 +1,41 @@
+package com.example.demo.domain.board
+
+import com.example.demo.commons.dto.Board
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class BoardAggregate(
+        @Id val id: String = UUID.randomUUID().toString(),
+        var title: String,
+
+        @Column(length = 2000, columnDefinition = "TEXT")
+        var content: String,
+
+        @Column(columnDefinition = "VARCHAR(100)")
+        var password: String,
+
+        @Column(columnDefinition = "VARCHAR(100)")
+        val issuerId: String
+) {
+    companion object {
+        fun ofRequest(createRequest: Board.BoardCreateRequest): BoardAggregate =
+                BoardAggregate(
+                        title = createRequest.title,
+                        issuerId = createRequest.issuerId,
+                        content = createRequest.content,
+                        password = createRequest.password
+                )
+    }
+
+    fun change(request: Board.ChangePasswordRequest) {
+        password = request.password
+    }
+
+    fun change(request: Board.ChangeBoardRequest) {
+        title = request.title
+        content = request.content
+    }
+}
