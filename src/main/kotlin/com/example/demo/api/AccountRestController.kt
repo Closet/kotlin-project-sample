5fc5cbcb7abb2b804ea5e85c557ae30879a4a41e
Closet <closset703@nate.com>
2020-01-01 19:59:43 +0900

    코드의 이름을 수정하고 레스트컨트롤러 단을 분리한다.
    work items : none

--- src/main/kotlin/com/example/demo/api/AccountRestController.kt
+++ src/main/kotlin/com/example/demo/api/AccountRestController.kt
@@ -6,27 +6,23 @@ import com.example.demo.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToMono
 
 @RestController
-class Controller(
+@RequestMapping("/api")
+class AccountRestController(
         private val userService: UserService,
         private val notificationService: NotificationService
 ) {
-    @GetMapping("/test")
-    fun test() = WebClient.create()
-            .get().uri("https://www.baeldung.com/").retrieve().bodyToMono<String>().log()
 
     @PostMapping("/user")
     fun postUser(@RequestBody accountRequest: CreateAccountRequest): ResponseEntity<Void> {
-        userService.joinAccount(accountRequest)
+        userService.createAccount(accountRequest)
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 
     @PatchMapping("/user")
     fun patchUser(@RequestBody accountRequest: CreateAccountRequest): ResponseEntity<Void> {
-        userService.joinAccount(accountRequest)
+        userService.createAccount(accountRequest)
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 }
