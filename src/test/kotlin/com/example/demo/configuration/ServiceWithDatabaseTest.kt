08917db4f85c47ce6d2d8a968b3200da63e23f37
Closet <closset703@nate.com>
2020-01-02 21:49:30 +0900

    프로퍼티 파일과 컨피규레이션을 수정한다
    프로퍼티 파일의 네이밍 이슈 수정
    - 프로퍼티 파일은 application-{name} 으로 명명해야한다
    
    컨피규레이션 수정
    - 위의 규칙에 맞게 수정하여 active 프로파일에 맞춰 정상적으로 인식 할 수 있도록 수정
    
    work items : none

--- src/test/kotlin/com/example/demo/configuration/ServiceWithDatabaseTest.kt
+++ src/test/kotlin/com/example/demo/configuration/ServiceWithDatabaseTest.kt
@@ -2,12 +2,10 @@ package com.example.demo.configuration
 
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestConstructor
-import org.springframework.test.context.TestPropertySource
 import org.springframework.transaction.annotation.Transactional
 
 
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-//@TestPropertySource("classpath:application-test.properties")
 @Transactional
 @SpringBootTest
 annotation class ServiceWithDatabaseTest
